d861e26c82d6fc8c9fecae9c5bf299775a1f1961
Yaro <22304756+yaromar@users.noreply.github.com>
2024-02-13 08:21:11 -0500

    Update example_usage.R

--- inst/scripts/example_usage.R
+++ inst/scripts/example_usage.R
@@ -4,6 +4,5 @@ library(SEMdetectR)
 
 
 DNA_methylation <- read.csv("path/to/your/data.csv") # Example data loading must be beta values; rows = subjects; columns = probes (with Illumina 450k names if using RF models)
-#mean imputation
 
 SEM_results <- detectSEM(DNA_methylation, num_cores = 4, rf = FALSE, cluster = FALSE)
