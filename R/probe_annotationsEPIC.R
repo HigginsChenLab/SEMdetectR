05887c84abed8e24c20ae316918aa51a589d97a0
Yaro <22304756+yaromar@users.noreply.github.com>
2024-06-20 00:22:16 -0400

    Create probe_annotationsEPIC.R

---
+++ R/probe_annotationsEPIC.R
@@ -0,0 +1,19 @@
+#' Illumina Probe Annotations Dataframe for the EPIC array
+#'
+#' A dataframe containing probe annotations used as features in the Random Forest models.
+#' Includes information on Name, Color, Source Sequence, presence of SNP at SBE, overlap with a differentially methylated region, Enhancer, regulatory features, DNase I hypersensitive sites, and type of genomic location of the probe.
+#' @name probe_annotationsEPIC.rda
+#' @format A data frame with 866836 rows and 9 columns:
+#' \describe{
+#'   \item{\code{Name}}{Name of Illumina probe.}
+#'   \item{\code{Color}}{Color of Illumina probe.}
+#'   \item{\code{SourceSeq}}{Nucleotide sequence.}
+#'   \item{\code{SBE_rs}}{SNPs at single-base extension.}
+#'   \item{\code{DMR}}{Differentially methylated region.}
+#'   \item{\code{Enhancer}}{Enhancer.}
+#'   \item{\code{regFeature}}{Regulatory features.}
+#'   \item{\code{DHS}}{DNase I hypersensitive sites.}
+#'   \item{\code{location}}{Genomic location.}
+#' }
+#' @source Taken from the Illumina manifest for the EPICk array
+"probe_annotations"
